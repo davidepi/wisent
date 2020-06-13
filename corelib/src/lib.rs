5218d40f7fdb4eb7194070a2a69389cebbbae857
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2020-06-13 23:35:40 +0900

    Initial commit

---
+++ corelib/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod core {
+    pub fn print_hello() {
+        println!("Hello World!");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core;
+
+    #[test]
+    fn it_works() {
+        core::print_hello();
+    }
+}
