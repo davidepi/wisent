4156d7a1b7e3a109bcd08b471abda598e8f2185d
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2022-09-08 02:39:11 +0900

    chore: simualtion benchmark, cleanup manifest

--- benches/generator_bench.rs
+++ benches/generator_bench.rs
@@ -1,9 +1,9 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::{BatchSize, Criterion};
 use wisent::grammar::Grammar;
-use wisent::lexer::Dfa;
+use wisent::lexer::{Dfa, DfaSimulator};
 
 fn parse_c_grammar(c: &mut Criterion) {
     c.bench_function("c grammar [parse grammar]", |b| {
@@ -16,5 +16,18 @@ fn lex_c_grammar(c: &mut Criterion) {
     c.bench_function("c grammar [lex grammar]", |b| b.iter(|| Dfa::new(&g)));
 }
 
-criterion_group!(benches, parse_c_grammar, lex_c_grammar);
+fn tokenize_c_file(c: &mut Criterion) {
+    let input = std::fs::read_to_string("./resources/c_example.txt").unwrap();
+    let g = Grammar::parse_grammar("./resources/c_grammar.txt").unwrap();
+    let dfa = Dfa::new(&g);
+    c.bench_function("c simulation [tokenizing]", |b| {
+        b.iter_batched(
+            || (DfaSimulator::new(dfa.clone()), input.chars()),
+            |(mut simulator, mut reader)| simulator.tokenize(&mut reader),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(benches, parse_c_grammar, lex_c_grammar, tokenize_c_file);
 criterion_main!(benches);
